58ca167a2471a2a437122d59d66b58ac04d423c1
project159085 <project159085@gmail.com>
2025-11-13 16:09:26 +0530

    Video Call Routing Started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
 import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
             <Route path='/' element={<LandingPage />} />
 
             <Route path='/auth' element={<Authentication />} />
+
+            <Route path='/:url' element={<VideoMeetComponent />} />
 
           </Routes>
 
