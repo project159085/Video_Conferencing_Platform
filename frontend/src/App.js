323393398da58b56b83a9e800d91c8b9ef06c65e
project159085 <project159085@gmail.com>
2025-11-16 22:07:16 +0530

    Created High Order Function For AUTH Guard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
 import { AuthProvider } from './contexts/AuthContext';
 import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/home';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
             <Route path='/' element={<LandingPage />} />
 
             <Route path='/auth' element={<Authentication />} />
+
+            <Route path='/home' element={<HomeComponent />} />
 
             <Route path='/:url' element={<VideoMeetComponent />} />
 
